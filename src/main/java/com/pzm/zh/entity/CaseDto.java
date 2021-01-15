c3ffc02ca7c99b6d1b46c55b6f5ff2f51b7cde76
2452072763@qq.com <Wcgo17855843046>
2021-01-15 15:15:11 +0800

    新增序号

--- src/main/java/com/pzm/zh/entity/CaseDto.java
+++ src/main/java/com/pzm/zh/entity/CaseDto.java
@@ -11,6 +11,8 @@ import java.util.Date;
  **/
 public class CaseDto extends Variables {
 
+    private String dataIndex; //序号
+
     private String caseires;//算法系列
 
     public CaseDto(Integer id, Integer REVISION, String CREATED_BY, Date CREATED_TIME, String UPDATED_BY, Date UPDATED_TIME, BigDecimal doorhigh, String style, BigDecimal doorweight, Integer doornums, String serizes, BigDecimal ztweight, BigDecimal plaThick, BigDecimal sztweight, BigDecimal bkweight, BigDecimal sxHigh, BigDecimal zxHigh, BigDecimal xxHigh, BigDecimal zdWeight, BigDecimal xbDepth, BigDecimal zxWeight, BigDecimal smWeight, BigDecimal xmWeight, BigDecimal glassDepth, BigDecimal zztWeight, BigDecimal xztWeight, String memo1, String memo2, String memo3, String memo4, String memo5, String memo6, Integer serizesId) {
@@ -20,9 +22,10 @@ public class CaseDto extends Variables {
     public CaseDto() {
     }
 
-    public CaseDto(Integer id, Integer REVISION, String CREATED_BY, Date CREATED_TIME, String UPDATED_BY, Date UPDATED_TIME, BigDecimal doorhigh, String style, BigDecimal doorweight, Integer doornums, String serizes, BigDecimal ztweight, BigDecimal plaThick, BigDecimal sztweight, BigDecimal bkweight, BigDecimal sxHigh, BigDecimal zxHigh, BigDecimal xxHigh, BigDecimal zdWeight, BigDecimal xbDepth, BigDecimal zxWeight, BigDecimal smWeight, BigDecimal xmWeight, BigDecimal glassDepth, BigDecimal zztWeight, BigDecimal xztWeight, String memo1, String memo2, String memo3, String memo4, String memo5, String memo6, Integer serizesId, String caseires) {
+    public CaseDto(Integer id, Integer REVISION, String CREATED_BY, Date CREATED_TIME, String UPDATED_BY, Date UPDATED_TIME, BigDecimal doorhigh, String style, BigDecimal doorweight, Integer doornums, String serizes, BigDecimal ztweight, BigDecimal plaThick, BigDecimal sztweight, BigDecimal bkweight, BigDecimal sxHigh, BigDecimal zxHigh, BigDecimal xxHigh, BigDecimal zdWeight, BigDecimal xbDepth, BigDecimal zxWeight, BigDecimal smWeight, BigDecimal xmWeight, BigDecimal glassDepth, BigDecimal zztWeight, BigDecimal xztWeight, String memo1, String memo2, String memo3, String memo4, String memo5, String memo6, Integer serizesId, String caseires,String dataIndex) {
         super(id, REVISION, CREATED_BY, CREATED_TIME, UPDATED_BY, UPDATED_TIME, doorhigh, style, doorweight, doornums, serizes, ztweight, plaThick, sztweight, bkweight, sxHigh, zxHigh, xxHigh, zdWeight, xbDepth, zxWeight, smWeight, xmWeight, glassDepth, zztWeight, xztWeight, memo1, memo2, memo3, memo4, memo5, memo6, serizesId);
         this.caseires = caseires;
+        this.dataIndex = dataIndex;
     }
 
     public CaseDto(String caseires) {
@@ -35,5 +38,13 @@ public class CaseDto extends Variables {
 
     public void setCaseires(String caseires) {
         this.caseires = caseires;
+    }
+
+    public String getDataIndex() {
+        return dataIndex;
+    }
+
+    public void setDataIndex(String dataIndex) {
+        this.dataIndex = dataIndex;
     }
 }
