22f8212dff650329c9a0c39b4ff78fe3ee2479d3
guochengwei <2452072763@qq.com>
2021-03-22 13:35:45 +0800

    修改bug

--- src/main/java/com/pzm/zh/service/DataGcServiceImpl.java
+++ src/main/java/com/pzm/zh/service/DataGcServiceImpl.java
@@ -963,8 +963,6 @@ public class DataGcServiceImpl implements DataGcService {
             length = new Double(2150);
         }else if (length.compareTo(new Double(2150)) >= 0 && length.compareTo(new Double(2200)) < 0) {
             length = new Double(2200);
-        }else if (length.compareTo(new Double(2150)) >= 0 && length.compareTo(new Double(2200)) < 0) {
-            length = new Double(2200);
         } else if (length.compareTo(new Double(2200)) >= 0 && length.compareTo(new Double(2250)) < 0) {
             length = new Double(2250);
         } else if (length.compareTo(new Double(2250)) >= 0 && length.compareTo(new Double(2300)) < 0) {
